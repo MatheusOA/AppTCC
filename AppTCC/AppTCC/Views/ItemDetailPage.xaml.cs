5514fade5593f1aebe6dd4df2451ff490e73aab0
Matheus Oliveira de Araujo <matheus.oliveira.94@outlook.com>
2020-08-28 22:21:09 -0300

    Adicionar arquivos de projeto.

---
+++ AppTCC/AppTCC/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AppTCC.ViewModels;
+
+namespace AppTCC.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
