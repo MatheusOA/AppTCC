a45ad55c5b4404df69ff84979e0fe7449a6e65b7
Matheus Oliveira de Araujo <matheus.oliveira.94@outlook.com>
2020-10-16 22:15:33 -0300

    Adicionado logo do app

--- AppTCC/AppTCC.Android/MainActivity.cs
+++ AppTCC/AppTCC.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AppTCC.Droid
 {
-    [Activity(Label = "AppTCC", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "EyeTractor", Icon = "@mipmap/LogoEyeTractor", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- AppTCC/AppTCC.Android/Resources/Resource.designer.cs
+++ AppTCC/AppTCC.Android/Resources/Resource.designer.cs
@@ -12516,6 +12516,9 @@ namespace AppTCC.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int LogoEyeTractor = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
